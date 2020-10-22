b446d10bb3ad5a9f37ca0ae22038fcff0eb60f13
paner28 <61655946+paner28@users.noreply.github.com>
2020-10-22 23:18:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
-from flask import Flask,request,abort
-from linebot import LineBotApi,WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent,TextMessage,TextSendMessage
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 import os
 
-app=Flask(__name__)
-#環境変数取得
-ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-line_bot_api = LineBotApi(ACCESS_TOKEN)
-handler = WebhookHandler(SECRET)
+app = Flask(__name__)
+app.debug = False
 
-@app.route("/callback",methods=["POST"])
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature=request.headers["X-Line-Signature"]
+    signature = request.headers['X-Line-Signature']
 
-    body=request.get_data(as_text=True)
-    app.logger.info("Request body"+body)
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
     try:
-        handler.handle(body,signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return "OK"
+    return 'OK'
 
-@handler.add(MessageEvent,message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
-if __name__=="__main__":
-    port=int(os.getenv("PORT",5000))
-    app.run(host="0.0.0.0",port=port)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
