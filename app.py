035ea5d3eb529104fd0124b5b5b84dfc8814555c
paner28 <61655946+paner28@users.noreply.github.com>
2020-10-23 11:36:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 import os
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,33 +8,32 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent,
-    ImageMessage, AudioMessage,
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
- #環境変数取得
+#環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
- #APIの設定
+#APIの設定
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
- #テスト用
+#テスト用
 @app.route("/")
 def hello_world():
     return "hello world!"
- #Webhookからのリクエストをチェック
+#Webhookからのリクエストをチェック
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     try:
-         handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-         abort(400)
+        abort(400)
     return 'OK'
- #オウム返しプログラム
+#オウム返しプログラム
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
